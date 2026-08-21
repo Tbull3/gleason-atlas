48f75ed35d64198ce895d31da8ecd2b9aa841066
Tbull3 <Tbull3@users.noreply.github.com>
2026-08-21 22:23:57 +0000

    Give the metric row the full width so Area is not under Search

--- src/components/atlas-app.tsx
+++ src/components/atlas-app.tsx
@@ -39,10 +39,8 @@ export function AtlasApp() {
             <AboutDialog />
           </div>
         </div>
-        <div className="flex flex-col gap-2 md:flex-row md:items-start">
-          <div className="min-w-0 flex-1">
-            <MetricSwitcher />
-          </div>
+        <MetricSwitcher />
+        <div className="md:flex md:justify-end">
           <HeaderSearch />
         </div>
       </header>
